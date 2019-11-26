3551fe98712819e1ccf4c75d1d814f718ecc837d
tbielaszewski <tomekbielaszewski@gmail.com>
2019-11-26 13:50:09 +0100

    creating entry tracking record out of new links array

--- handler.js
+++ handler.js
@@ -4,25 +4,32 @@ const tracking = require('./lib/entry-tracking');
 const getNewLinks = require('./lib/wykop-link-provider');
 const formatEmail = require('./lib/email-formatter');
 const sendEmail = require('./lib/email-sender');
+const _ = require('lodash');
 
 module.exports.run = (event, context, callback) => {
   tracking.get()
-    .then(getNewLinks)
-    .then(links => {
-      return formatEmail(links)
-        .then(sendEmail)
-        .then(_ => {
-          return tracking.save(toTracking(links))
-        })
-        .then(_ => {
-          finish(callback);
-        })
-        .catch(error(callback));
+    .then(entryTracking => {
+      return getNewLinks(entryTracking)
+        .then(newLinks => {
+          return formatEmail(newLinks)
+            .then(sendEmail)
+            .then(_ => {
+              return tracking.save(toTracking(entryTracking, newLinks))
+            })
+            .then(_ => {
+              finish(callback);
+            })
+            .catch(error(callback));
+        });
     });
 };
 
-function toTracking(newLinks) {
-  return undefined;
+function toTracking(oldEntryTracking, newLinks) {
+  let newestLinks = _.values(_.groupBy(newLinks, 'tag'))
+    .map(link => _.maxBy(link, l => new Date(l.date).getTime()));
+  return _.values(_.groupBy(oldEntryTracking, 'tag'))
+    .map(t => t[0])
+    .map(t => _.set(t, 'lastDate', _.find(newestLinks, l => l.tag === t.tag).date))
 }
 
 function finish(callback) {
